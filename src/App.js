05011ce5bc4df148ee7e2ffff4e4ec0c9b0f3563
DwarakanathU <dwarakanathummadisetti@gmail.com>
2022-08-24 19:45:03 +0530

    created folder structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
+  const title = "Tic Tac Toe";
   return (
     <div className="container">
       <section>
-        <header>Tic Tac Toe</header>
-        <main></main>
+        <Header title={title} />
+        <main>Playing table</main>
+        <section>buttons</section>
       </section>
-      <section></section>
+      <section>
+        <main>History</main>
+      </section>
     </div>
   );
 }
